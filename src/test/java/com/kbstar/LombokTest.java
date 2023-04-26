ee2763ca83d8b03afad1b0bf025098f115fe27aa
sungyoung1107 <dong09161107@gmail.com>
2023-04-26 09:15:44 +0900

    2023-04-26

---
+++ src/test/java/com/kbstar/LombokTest.java
@@ -0,0 +1,22 @@
+package com.kbstar;
+
+import com.kbstar.dto.Cust;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LombokTest {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
+//    int i = 10;
+    @Test
+    void contextLoad() {
+
+//        System.out.println("i는 "+ i); // 절대 쓰지 말자
+        Cust cust = new Cust("id01", "pwd01", "이말숙");
+        logger.info(cust.toString());
+    }
+}
---
+++ src/test/java/com/kbstar/Temp08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Temp08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
