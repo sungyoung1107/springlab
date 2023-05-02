b992f744b0c2e833f6498dc70a82ec690f7fc64e
sungyoung1107 <dong09161107@gmail.com>
2023-05-02 09:07:43 +0900

    2023-05-02

---
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,58 @@
+package com.kbstar.controller;
+
+import com.kbstar.dto.Cust;
+import com.kbstar.service.CustService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+    @Autowired
+    CustService custService;
+    @RequestMapping("/")
+    public String main(){
+        return "index";
+    }
+    @RequestMapping("/login")
+    public String login(Model model){
+        model.addAttribute("center", "login");
+        return "index";
+    }
+
+    @RequestMapping("/loginimpl")
+    public String register(Model model, String id, String pwd){
+        logger.info("-----" + id+"이고, "+pwd + "-----");
+        model.addAttribute("center", "login");
+        return "index";
+    }
+
+    @RequestMapping("/register")
+    public String register(Model model){
+        model.addAttribute("center", "register");
+        return "index";
+    }
+
+    @RequestMapping("/registerimpl")
+    public String register(Model model, Cust cust) throws Exception{
+        try {
+            custService.register(cust);
+        } catch(Exception e){
+            throw new Exception("가입오류");
+        }
+        model.addAttribute("rcust", cust);
+        model.addAttribute("center", "registerok");
+        return "index";
+    }
+
+    // /quics?page=bs01
+    @RequestMapping("/quics")
+    public String quics(String page){
+        return page;
+    }
+
+}
